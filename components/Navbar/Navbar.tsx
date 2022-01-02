671cef31ec29476821371ffd03073f38f6053715
Seb Dybowski <dybowski.seb@gmail.com>
2022-01-02 21:13:50 +0100

    navbar implementation 2

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
-import styles from './Navbar.module.css';
+import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTerminal } from '@fortawesome/free-solid-svg-icons'
+import styles from './Navbar.module.css';
 
 const Navbar = () => (
   <div className={styles.Navbar} data-testid="Navbar">
     <div className={styles.Line} />
-    <FontAwesomeIcon
-      icon={faTerminal}
-      size='xs'
-      className={styles.Icon}
-    />
-    <div className={styles.Logo}>sebdybowski</div>
+    <Link href="/">
+      <a className={styles.Logo}>
+        <div className={styles.IconWrapper}>
+          <FontAwesomeIcon
+            icon={faTerminal}
+            size='1x'
+            className={styles.Icon}
+          />
+        </div>
+        <span>seb<span className={styles.Surname}>dybowski</span></span>
+      </a>
+    </Link>
     <div className={styles.Line} />
-    <div className={styles.Links}>Links</div>
+    <ul className={styles.Links}>
+      <li>home</li>
+      <li>skills</li>
+      <li>contact</li>
+    </ul>
     <div className={styles.Line} />
   </div>
 );
