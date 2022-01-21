61645b5a3ec95d89c568c45006b52516b6d82ba0
Seb Dybowski <dybowski.seb@gmail.com>
2022-01-21 22:33:44 +0100

    navbar doodling 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import Footer from '@/components/Footer/Footer'
-import Navbar from '@/components/Navbar/Navbar'
+import Footer from '@/pages/components/Footer/Footer'
+import Navbar from '@/pages/components/Navbar/Navbar'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
