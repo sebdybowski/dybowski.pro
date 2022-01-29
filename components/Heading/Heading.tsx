3adccc4da97a48e651207a4e55fdc54f81e74efd
Seb Dybowski <dybowski.seb@gmail.com>
2022-01-29 18:30:46 +0100

    headers and containers

--- components/Heading/Heading.tsx
+++ components/Heading/Heading.tsx
@@ -1,12 +1,15 @@
-import React, {ReactNode} from 'react';
+import React, {ReactNode, createElement} from 'react';
 import styles from './Heading.module.css';
 
-type HeadingProps = { children: ReactNode };
+type HeadingProps = {
+  children: ReactNode,
+  level?: 1 | 2 | 3 | 4 | 5 | 6
+};
 
-const Heading = ({ children }: HeadingProps) => (
-  <div className={styles.Heading} data-testid="Heading">
-    {children}
-  </div>
+const Heading = ({ level = 1, children }: HeadingProps) => createElement(
+  `h${level}`,
+  { className: styles.Heading, ['data-testid']: 'Heading'},
+  children
 );
 
 export default Heading;
