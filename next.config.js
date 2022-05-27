99f63791d3cc059373c6376b06ffd2bd0872c459
Seb Dybowski <dybowski.seb@gmail.com>
2022-05-27 20:46:06 +0200

    temp fix for tests

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { dev }) => {
+    config.module.rules.push(
+      {
+        test: /\.test.js$/,
+        loader: 'ignore-loader'
+      }
+    );
+    return config;
+  }
 }
