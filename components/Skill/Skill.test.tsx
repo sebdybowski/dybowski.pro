55784571cd92aa1171d72e77f1ae30f16ebf1663
Seb Dybowski <dybowski.seb@gmail.com>
2022-06-09 19:30:44 +0200

    cleanup scss

--- components/Skill/Skill.test.tsx
+++ components/Skill/Skill.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 
-import '@testing-library/jest-dom/extend-expect';
 import Skill from './Skill';
 
 describe('<Skill />', () => {
