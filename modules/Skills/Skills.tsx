7fca321ad80689dbf9b3cc6aed24e1b7f6e14cf5
Seb Dybowski <dybowski.seb@gmail.com>
2022-07-23 21:25:42 +0200

    skills revamp

--- modules/Skills/Skills.tsx
+++ modules/Skills/Skills.tsx
@@ -10,12 +10,13 @@ import styles from './Skills.module.scss';
 interface SkillsProps { }
 
 const Skills: FC<SkillsProps> = () => (
-  <div className={styles.Hero} data-testid="Skills">
+  <div className={styles.Skills} data-testid="Skills">
     <div className='container'>
       <br />
-      <div className='grid'>
-        <h2>Skills</h2>
-      </div>
+        <h2># Skills</h2>
+        <p>
+          Check my skillset!
+        </p>
       <div className='grid'>
         <article>
           <header>Back-end</header>
@@ -26,20 +27,25 @@ const Skills: FC<SkillsProps> = () => (
         <article>
           <header>Front-end</header>
           <ul>
-            <li>Javascript <progress value="90" max="100"></progress></li>
             <li>
-              <FontAwesomeIcon
-                size='1x'
-                icon={faReact}
-              />
-              React <strong>90%</strong>
-              <progress value="100" max="100"></progress></li>
+              JavaScript <strong>90%</strong>
+              <progress value="90" max="100"></progress>
+            </li>            <li>
+              React <strong>100%</strong>
+              <progress value="100" max="100"></progress>
+            </li>
+            <li>
+              Vue <strong>50%</strong>
+              <progress value="50" max="100"></progress>
+            </li>
           </ul>
         </article>
         <article>
           <header>UI/UX</header>
           <ul>
-            <li>React</li>
+            <li>Figma</li>
+            <li>A/B testing</li>
+            <li>Prototyping</li>
           </ul>
         </article>
       </div>
