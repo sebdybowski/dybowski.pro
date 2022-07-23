a9f52fe0cb2b13fa77ea5046add74389035186c8
Seb Dybowski <dybowski.seb@gmail.com>
2022-07-23 19:29:39 +0200

    current state

--- modules/Skills/Skills.tsx
+++ modules/Skills/Skills.tsx
@@ -11,118 +11,39 @@ interface SkillsProps { }
 
 const Skills: FC<SkillsProps> = () => (
   <div className={styles.Hero} data-testid="Skills">
-    <Container section>
-      <Column vCenter>
-        <Container fluid>
-          <Column hCenter>
-            <Heading level={1} serif brand >Skills</Heading>
-          </Column>
-        </Container>
-        <Container fluid>
-          <Column hCenter>
-            <Heading level={2}>Front-end</Heading>
-            <Container fluid>
-              <Column hCenter>
-                <FontAwesomeIcon
-                  size='2x'
-                  icon={faJs}
-                />
-                <span>JavaScript</span>
-              </Column>
-              <Column hCenter>
-                <FontAwesomeIcon
-                  size='2x'
-                  icon={faReact}
-                />
-                <span>React.js</span>
-              </Column>
-              <Column hCenter>
-                <FontAwesomeIcon
-                  size='2x'
-                  icon={faVuejs}
-                />
-                <span>Vue.js</span>
-              </Column>
-            </Container>
-            <Container fluid>
-              <Column hCenter>
-                <FontAwesomeIcon
-                  size='2x'
-                  icon={faAngular}
-                />
-                <span>Angular.js</span>
-              </Column>
-              <Column hCenter>
-                <FontAwesomeIcon
-                  size='2x'
-                  icon={faCss3Alt}
-                />
-                <span>CSS3</span>
-              </Column>
-              <Column hCenter>
-                <FontAwesomeIcon
-                  size='2x'
-                  icon={faHtml5}
-                />
-                <span>HTML5</span>
-              </Column>
-            </Container>
-          </Column>
-          <Column hCenter>
-            <Heading level={2}>Back-end</Heading>
-            <Container fluid>
-              <Column hCenter>
-                <FontAwesomeIcon
-                  size='2x'
-                  icon={faNode}
-                />
-                <span>Node.js</span>
-              </Column>
-              <Column hCenter>
-                <FontAwesomeIcon
-                  size='2x'
-                  icon={faJava}
-                />
-                <span>Java</span>
-              </Column>
-              <Column hCenter>
-                <FontAwesomeIcon
-                  size='2x'
-                  icon={faAws}
-                />
-                <span>AWS</span>
-              </Column>
-            </Container>
-          </Column>
-          <Column hCenter>
-            <Heading level={2}>UI/UX</Heading>
-            <Container fluid>
-              <Column hCenter>
-                <FontAwesomeIcon
-                  size='2x'
-                  icon={faFigma}
-                />
-                <span>Figma</span>
-              </Column>
-              <Column hCenter>
-                <FontAwesomeIcon
-                  size='2x'
-                  icon={faObjectGroup}
-                />
-                <span>Mockups</span>
-              </Column>
-              <Column hCenter>
-                <FontAwesomeIcon
-                  size='2x'
-                  icon={faCompassDrafting}
-                />
-                <span>A/B Testing</span>
-              </Column>
-            </Container>
-          </Column>
-        </Container>
-      </Column>
-    </Container>
+    <div className='container'>
+      <br />
+      <div className='grid'>
+        <h2>Skills</h2>
+      </div>
+      <div className='grid'>
+        <article>
+          <header>Back-end</header>
+          <ul>
+            <li>Java</li>
+          </ul>
+        </article>
+        <article>
+          <header>Front-end</header>
+          <ul>
+            <li>Javascript <progress value="90" max="100"></progress></li>
+            <li>
+              <FontAwesomeIcon
+                size='1x'
+                icon={faReact}
+              />
+              React <strong>90%</strong>
+              <progress value="100" max="100"></progress></li>
+          </ul>
+        </article>
+        <article>
+          <header>UI/UX</header>
+          <ul>
+            <li>React</li>
+          </ul>
+        </article>
+      </div>
+    </div>
   </div>
 );
 
