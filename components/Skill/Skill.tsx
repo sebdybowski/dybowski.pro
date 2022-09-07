e6c1340c4689adb85958307ba4a6bd39579f5741
Seb Dybowski <dybowski.seb@gmail.com>
2022-09-07 22:30:04 +0200

    styling fixes

--- components/Skill/Skill.tsx
+++ components/Skill/Skill.tsx
@@ -1,22 +1,27 @@
-import clsx from 'clsx';
 import React, { FC, MouseEventHandler } from 'react';
 
-import styles from './Skill.module.scss';
-
-interface SkillProps {
+interface Skill {
   label: string,
-  percent: number,
-  onClick: MouseEventHandler<HTMLButtonElement>
+  percent: number
 }
 
-const Skill: FC<SkillProps> = ({ label, percent, onClick }) => (
-  <button onClick={onClick} data-testid="Skill">
-    {/* @ts-ignore */}
-    <div className={clsx(styles.Skill, styles.pie)} style={{ ['--p']: percent }}>
-      <span className={styles.Label}>{label}</span>
-      <span className={styles.Percentage}>{`${percent}%`}</span>
-    </div>
-  </button>
+interface SkillProps {
+  header: string,
+  skills: Skill[]
+}
+
+const Skill: FC<SkillProps> = ({ header, skills }) => (
+  <article>
+    <header>{header}</header>
+    <ul>
+      {skills.map(({ label, percent }) => (
+        <li>
+          {label} <strong>{`${percent}%`}</strong>
+          <progress value={percent} max="100"></progress>
+        </li>
+      ))}
+  </ul>
+  </article >
 );
 
 export default Skill;
