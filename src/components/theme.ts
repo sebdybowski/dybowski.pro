b537b8ac113db87ccbc203da6d970e587bf1fda8
Seb Dybowski <dybowski.seb@gmail.com>
2022-09-15 19:07:02 +0200

    theme switcher improvements

---
+++ src/components/theme.ts
@@ -0,0 +1,9 @@
+const THEME = {
+    LIGHT: "light",
+    DARK: "dark",
+};
+
+const SWITCHER_ID = "theme-switcher";
+const DATA_ATTRIBUTE = "data-theme";
+
+export { THEME, SWITCHER_ID, DATA_ATTRIBUTE };
