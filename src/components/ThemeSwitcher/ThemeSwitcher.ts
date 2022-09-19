9a96915a2c2542fef3e1ca7eb100eb25cc1dc330
Seb Dybowski <dybowski.seb@gmail.com>
2022-09-19 14:10:18 +0200

    blob

---
+++ src/components/ThemeSwitcher/ThemeSwitcher.ts
@@ -0,0 +1,29 @@
+const THEME = {
+  LIGHT: 'light',
+  DARK: 'dark'
+};
+
+const SWITCHER_ID = 'theme-switcher';
+const DATA_ATTRIBUTE = 'data-theme';
+
+const initThemeSwitcher = (): void => {
+  const button = document.getElementById(SWITCHER_ID);
+  const html = document.getElementsByTagName('html')[0];
+
+  if (button) {
+    button.onclick = (): void => {
+      const theme = html.getAttribute(DATA_ATTRIBUTE);
+
+      if (theme === THEME.LIGHT) html.setAttribute(DATA_ATTRIBUTE, THEME.DARK);
+      if (theme === THEME.DARK) html.setAttribute(DATA_ATTRIBUTE, THEME.LIGHT);
+    };
+
+    button.addEventListener('click', (): void => {
+      const theme = html.getAttribute(DATA_ATTRIBUTE);
+
+      if (theme) button.setAttribute('class', theme);
+    });
+  }
+};
+
+export { THEME, SWITCHER_ID, DATA_ATTRIBUTE, initThemeSwitcher };
--- src/components/theme.ts
+++
@@ -1,9 +0,0 @@
-const THEME = {
-    LIGHT: "light",
-    DARK: "dark"
-};
-
-const SWITCHER_ID = "theme-switcher";
-const DATA_ATTRIBUTE = "data-theme";
-
-export { THEME, SWITCHER_ID, DATA_ATTRIBUTE };
