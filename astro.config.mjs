bac61692a5695543b16f8c58331a4319494affd9
Seb Dybowski <dybowski.seb@gmail.com>
2023-07-13 13:34:00 +0200

    OoL improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-import preact from "@astrojs/preact"; // https://astro.build/config
 
-import image from "@astrojs/image";
+import preact from '@astrojs/preact';
+import image from '@astrojs/image';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), image()]
+  site: 'https://dybowski.pro',
+  integrations: [preact(), image(), sitemap()]
 });
