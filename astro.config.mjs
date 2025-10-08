96db3c44e205869bd03ed065ebb80a3f4a6f486a
Seb Dybowski <dybowski.seb@gmail.com>
2025-10-08 18:22:12 +0200

    lib update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dybowski.pro',
-  integrations: [preact(), sitemap()]
+  integrations: [preact(), sitemap(), icon()]
 });
