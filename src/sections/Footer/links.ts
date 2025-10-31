c9c7607378211150ffa3522c852b32039b11746d
Sebastian Dybowski <dybowski.seb@gmail.com>
2025-10-31 16:13:32 +0100

    node + astro update (#196)

---
+++ src/sections/Footer/links.ts
@@ -0,0 +1,34 @@
+const connect = [
+    {
+        url: "https://github.com/sebdybowski",
+        label: "Github",
+        icon: "lucide:github",
+    },
+    {
+        url: "https://pl.linkedin.com/in/sebdybowski",
+        label: "LinkedIn",
+        icon: "lucide:linkedin",
+    },
+    {
+        url: "https://twitter.com/sebdybowskidev",
+        label: "Twitter (X)",
+        icon: "lucide:twitter",
+    },
+];
+
+const contact = [
+    {
+        label: "Mobile",
+        value: "+48 782 710 459",
+        url: "tel:+48782710459",
+        icon: "lucide:phone",
+    },
+    {
+        label: "E-mail",
+        value: "dybowski.seb@gmail.com",
+        url: "mailto:dybowski.seb@gmail.com",
+        icon: "lucide:mail",
+    },
+];
+
+export { connect, contact };
